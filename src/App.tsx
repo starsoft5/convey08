fc32c6330fc30723fbd19f26926daf28c353dc78
e-a-11 <apollo_estrella@yahoo.com>
2025-07-01 14:48:51 +0800

    ci cd 07

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          God wil make a way !
+          God wil make a way, when there seems to be a way !!! 
         </p>
         <a
           className="App-link"
